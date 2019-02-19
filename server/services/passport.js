21f7c1b48ef59f390f08d8933d8f97eb93494699
Chris Anderson <chrisanderson@Chris-Andersons-MacBook-Pro.local>
2019-02-19 10:26:40 -0500

    added serialization and cookie keys

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,25 +5,38 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user.id);
+        });
+})
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
 
-    User.find({googleId: profile.id})
-    .then((existingUser) => {
-        if(existingUser){
-            //we have a record already
-            console.log('user exists');
-            //error, user
-            done(null, existingUser);
-        }
-        else {
-            new User({ googleId: profile.id }).save()
-            .then(user => done(null, user));
+    User.find({
+            googleId: profile.id
+        })
+        .then((existingUser) => {
+            if (existingUser) {
+                //we have a record already
+                console.log('user exists');
+                //error, user
+                done(null, existingUser);
+            } else {
+                new User({
+                        googleId: profile.id
+                    }).save()
+                    .then(user => done(null, user));
 
-        }
-    });
-})
-);
+            }
+        });
+}));
