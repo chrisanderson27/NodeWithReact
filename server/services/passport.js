b8371c6c4d6d60ca16e6859a06cd62db0d306fac
Chris Anderson <chrisanderson@Chris-Andersons-MacBook-Pro.local>
2019-02-19 12:52:37 -0500

    prod and dev envs

--- server/services/passport.js
+++ server/services/passport.js
@@ -12,7 +12,9 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((id, done) => {
     User.findById(id)
         .then(user => {
-            done(null, user.id);
+            if (user) {
+                done(null, user.id);
+            }
         });
 })
 
