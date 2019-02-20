3de0da0ddfa753cc731079c0a9a5e38b8b0ef875
Chris Anderson <chrisanderson@Chris-Andersons-MacBook-Pro.local>
2019-02-20 14:32:53 -0500

    dev and prod config

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,9 +1,7 @@
 import { FETCH_USER } from "../actions/types";
 
 export default function(state = null, action) {
-  console.log("authReducer.js");
 
-  console.log('!!',action);
 
   switch (action.type) {
     case FETCH_USER:
