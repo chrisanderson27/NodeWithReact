361bdbf89324906d47207c6d689087657f7d0554
Chris Anderson <chrisanderson@Chris-Andersons-MacBook-Pro.local>
2019-02-20 16:18:09 -0500

    added middlewear for login check

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,32 +1,31 @@
-const passport = require("passport");
+const passport = require('passport');
 
 module.exports = app => {
-  app.get("/", (req, res) => {
-    res.send("this is just a test");
+  app.get('/', (req, res) => {
+    res.send('this is just a test');
   });
 
   app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-      scope: ["profile", "email"],
-      prompt: "select_account"
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email'],
+      prompt: 'select_account'
     })
   );
 
   app.get(
-    "/auth/google/callback",
-    passport.authenticate("google"),
+    '/auth/google/callback',
+    passport.authenticate('google'),
     (req, res) => {
-      res.redirect("/surveys");
+      res.redirect('/surveys');
     }
   );
 
-  app.get("/api/current_user", (req, res) => {
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
-  app.get("/api/logout", (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
-    res.redirect("/");
+    res.redirect('/');
   });
-
 };
--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,21 +1,26 @@
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-const keys = require("../config/keys");
-const stripe = require("stripe")(keys.stripeSecretKey);
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogins');
 
 // Token is created using Checkout or Elements!
 // Get the payment token ID submitted by the form:
-const token = request.body.stripeToken; // Using Express
-
-(async () => {
-  const charge = await stripe.charges.create({
-    amount: 999,
-    currency: "usd",
-    description: "Example charge",
-    source: token
-  });
-})();
+// const token = request.body.stripeToken; // Using Express
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {});
+  app.post('/api/stripe', requireLogin, async (req, res) => {
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: 'buy 5 credits',
+      source: req.body.id
+    });
+    console.log('charge: ', charge);
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
+  });
 };
